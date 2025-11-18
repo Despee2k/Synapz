51111c5969b72bb793a6b72da44abbeff8c907cc
Dempsey <dagantareynat@gmail.com>
2025-11-18 11:28:35 +0800

    feat: added new quizzes and removed old

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -13,7 +13,7 @@ import {
 import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Brain, Play, BookOpen } from 'lucide-react';
 
-const NOTICE_VERSION = '2025-10-25-a'; // BUMP THIS FOR NOTICE
+const NOTICE_VERSION = '2025-11-28-a'; // BUMP THIS FOR NOTICE
 const NOTICE_STORAGE_KEY = 'release_notice_version';
 
 export default function Home() {
@@ -184,8 +184,7 @@ export default function Home() {
             <DialogTitle>Notice</DialogTitle>
           </DialogHeader>
           <ul className="text-sm text-slate-600 list-disc pl-5 space-y-1">
-            <li>Added OS quizzes (1-4).</li>
-            <li>Added IAS quizzes (12-16).</li>
+            <li>Added IAS quizzes (17-22).</li>
           </ul>
         </DialogContent>
       </Dialog>
